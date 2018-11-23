b7982ea61dab1c4afe88fac3d9d064ffc2d1f878
smtardast <smtardast@gmail.com>
2018-11-23 15:04:02 +0100

    intro fini

---
+++ assets/main.js
@@ -0,0 +1,17 @@
+// var text = new Blotter.Text("Sara Tardast", {
+//     family: "Arial",
+//     size: 30,
+//     fill: "#e4ef09",
+// });
+
+// var material = new Blotter.Material();
+
+// var blotter = new Blotter(material, {
+//     texts: text
+// });
+
+// var elem = document.getElementById("here");
+
+// var scope = blotter.forText(text);
+
+// scope.appendTo(elem);
